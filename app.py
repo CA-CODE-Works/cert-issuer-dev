9c48e79cfc44a858d4473e2956b620e5d491d17e
ssiwan <convicx@gmail.com>
2018-10-04 16:39:21 -0700

    add app.py

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-#!/usr/bin/python
-
-from flask import Flask
+#!/usr/bin/python3
+import json
+from flask import Flask, jsonify, request, abort
 from subprocess import call
 
-app = Flask(__name__)
+import cert_issuer.config
+from cert_issuer.blockchain_handlers import bitcoin
+import cert_issuer.issue_certificates
 
+app = Flask(__name__)
+config = None
+
+def get_config():
+    global config
+    if config == None:
+        config = cert_issuer.config.get_config()
+    return config
+
+@app.route('/cert_issuer/api/v1.0/issue', methods=['POST'])
+def issue():
+    config = get_config()
+    certificate_batch_handler, transaction_handler, connector = \
+            bitcoin.instantiate_blockchain_handlers(config, False)
+    certificate_batch_handler.set_certificates_in_batch(request.json)
+    cert_issuer.issue_certificates.issue(config, certificate_batch_handler, transaction_handler)
+    return json.dumps(certificate_batch_handler.proof)
 
 @app.route('/')
 def hello_world():
     call(["/bin/bash", "test.sh"])
     return ""
+
+
 
 
 if __name__ == "__main__":
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ if [ $(echo "$1" | cut -c1) = "-" ]; then
   set -- bitcoind "$@"
 fi
 
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bitcoind" ]; then
   mkdir -p "$BITCOIN_DATA"
   chmod 700 "$BITCOIN_DATA"
   chown -R bitcoin "$BITCOIN_DATA"
@@ -14,12 +15,15 @@ fi
   echo "$0: setting data directory to $BITCOIN_DATA"
 
   set -- "$@" -datadir="$BITCOIN_DATA"
+fi
 
-if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then  
-echo
+if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
+  echo
   exec gosu bitcoin "$@"
 fi
 
-# bitcoind -daemon --testnet
+
+bitcoind -regtest -daemon
+
 echo
 exec "$@"
