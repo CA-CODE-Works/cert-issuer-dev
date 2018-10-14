fc363b82293891d1a383512a29fac32016b38cce
Shamal Siwan <shamal.siwan@state.ca.gov>
2018-10-14 02:30:40 -0700

    Remove certs

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ echo "hello world"
 bitcoin-cli getbalance
 bitcoin-cli sendtoaddress $issuer 5
 cert-issuer -c /etc/cert-issuer/conf.ini
-echo "Certs Issued already"
+rm /etc/cert-issuer/data/unsigned_certificates/*
+echo "Certs Issued"
 
 exit 0
